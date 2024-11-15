e832e9572d6e8e070e166f0a41f30b06df1aafaf
coraliehaller <haller.coralie.15@gmail.com>
2024-11-15 14:51:42 +0100

    Impossible d'utiliser Lombok pour mon model User -> je l'enlève

--- src/main/java/com/openclassroom/ChatopApiApplication.java
+++ src/main/java/com/openclassroom/ChatopApiApplication.java
@@ -3,10 +3,11 @@ package com.openclassroom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = "com.openclassroom")
 @EnableJpaRepositories(basePackages = "com.openclassroom.repositories")
-
+@EntityScan(basePackages = "com.openclassroom.models")
 public class ChatopApiApplication {
 
 	public static void main(String[] args)
--- src/main/java/com/openclassroom/models/User.java
+++ src/main/java/com/openclassroom/models/User.java
@@ -1,50 +1,63 @@
 package com.openclassroom.models;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.*;
-import javax.persistence.Entity;
 
 @Entity
-@Table(name = "USERS") // Assure-toi que le nom correspond à ta base de données
+@Table(name = "USERS")
 public class User {
 
-    @Setter
-    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
-    private String name;
+    private String username;
 
-    @Getter
-    @Setter
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Setter
-    @Getter
     @Column(nullable = false)
     private String password;
 
-    // Constructeurs, getters et setters
-    public User() {
-    }
+    // Constructeurs
+    public User() {}
 
-    public User(String name, String email, String password) {
-        this.name = name;
+    public User(String username, String email, String password) {
+        this.username = username;
         this.email = email;
         this.password = password;
     }
 
+    // Getters et Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getUsername() {
-        return name;
+        return username;
     }
 
-    public void setUsername(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
