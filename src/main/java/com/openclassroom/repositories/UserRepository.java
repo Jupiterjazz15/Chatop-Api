c43e588ed80408a7b11ec6391c6584175c86eb5d
coraliehaller <haller.coralie.15@gmail.com>
2024-11-16 15:20:27 +0100

    Repasse sur UserRepository

--- src/main/java/com/openclassroom/repositories/UserRepository.java
+++ src/main/java/com/openclassroom/repositories/UserRepository.java
@@ -1,30 +1,34 @@
-package com.chatop.api.repository;
+package com.openclassroom.repositories;
 
+import org.springframework.stereotype.Repository;
+// annotation @Repository, indique que cette interface est un composant Spring pour gérer l'accès aux données.
+import org.springframework.data.jpa.repository.JpaRepository;
+// fournit l'interface JpaRepository incluant les mthds pr interagir avec la DB (CRUD, etc.).
 import java.util.Optional;
+// utilisé pr éviter les retours null et indiquer qu'une valeur peut ou non être présente
+import com.openclassroom.models.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.chatop.api.models.User;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
+// ATTENTION COCO : nécessaire ??
+// annotation @Autowired qui permet d'injecter automatiquement des dépendances Spring
 import org.springframework.data.jpa.repository.Query;
+// annotation @Query utilisée pour définir des requêtes SQL personnalisées.
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+// annotation @Param utilisée pour lier les paramètres des requêtes à leurs valeurs.
 
-@Repository
+@Repository // interface UserRepository gère l'accès aux données.
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+    // mthds de JpaRepository
+    Optional<User> findByEmail(String email);
+    // mthds de JpaRepository
     Optional<User> findById(long id);
-
+    // mthds de JpaRepository pour éviter les doublons
     Boolean existsByUsername(String username);
-
+    // mthds de JpaRepository pour éviter les doublons
     Boolean existsByEmail(String email);
 
-    @Autowired
-    @Query(value = "SELECT * FROM users_table WHERE email = :email", nativeQuery = true)
-    Optional<User> findByEmail(@Param("email") String email);
+    // mthds qui n'est pas dans JpaRepository donc on la créé à la main  // ATTENTION COCO : nécessaire ??
+    // @Autowired // ATTENTION COCO : nécessaire ??
+    // @Query(value = "SELECT * FROM users WHERE email = :email", nativeQuery = true)
+    // Optional<User> findByEmail(@Param("email") String email);
 }
