3f497d6182cb868de115a3514d0cc4d4ca7a7699
coraliehaller <haller.coralie.15@gmail.com>
2024-11-22 14:42:52 +0100

    Ajustement des commentaires UserDetailsServicesImpl

--- src/main/java/com/openclassroom/security/services/UserDetailsServicesImpl.java
+++ src/main/java/com/openclassroom/security/services/UserDetailsServicesImpl.java
@@ -1,21 +1,14 @@
 package com.openclassroom.security.services;
 
-import org.springframework.stereotype.Service;
-// @Service => indiquer que c'est un composant Spring de type service
-import org.springframework.security.core.userdetails.UserDetailsService;
-// Interface de Spring Security utilisée pour récupérer les infos d'un utilisateur pr l'authentification
-import com.openclassroom.repositories.UserRepository;
-// mon UserRepository
-import org.springframework.beans.factory.annotation.Autowired;
-// @Autowired => nécessaire à l'injection de dépendance
-import org.springframework.security.core.userdetails.UserDetails;
-// Interface de Spring Security pr représenter les infos d'un utilisateur pr l'authentification
-import com.openclassroom.models.User;
-// mon modèle User
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-// une classe héritant de RuntimeException de SSECU utilisée pr signaler qu'un user n'a pas été trouvé lors de la tentative de récupération de ses infos.
+import org.springframework.stereotype.Service; // annotation indiquant que ce composant Spring est de type service
+import org.springframework.security.core.userdetails.UserDetailsService; // Interface de SSECU utilisée pr récupérer les infos d'un user pr l'authentification
+import com.openclassroom.repositories.UserRepository;// interface dont le but est de fournir les opérations nécessaires pour interagir avec DB
+import org.springframework.beans.factory.annotation.Autowired; // annotation nécessaire à l'injection de dépendance
+import org.springframework.security.core.userdetails.UserDetails; // interface de SSECU pr représenter les infos d'un user
+import com.openclassroom.models.User;// ma classe réprésentant les caractéristiques et les données liées à un user
+import org.springframework.security.core.userdetails.UsernameNotFoundException; // une classe héritant de RuntimeException de SSECU utilisée pr signaler qu'un user n'a pas été trouvé lors de la tentative de récupération de ses infos.
 
-@Service // class de Spring de type service
+@Service
 public class UserDetailsServicesImpl implements UserDetailsService {
 
     @Autowired  // Injection de dépendance : permet à Spring de fournir une instance de UserRepository.
@@ -23,13 +16,9 @@ public class UserDetailsServicesImpl implements UserDetailsService {
 
     @Override // on override la mthd loadUserByUsername de SSECU pour faire une recherche par mail
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
-                //  on cherche une instance existante User en utilisant la méthode findByEmail(email) de notre UserRepository
+        User user = userRepository.findByEmail(email) // cherche l'instance User existante en utilisant la mthd findByEmail() de notre UserRepository
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + email));
-        // si je ne trouve pas l'instance existante de User, une instance de UsernameNotFoundException est créée avec le message spécifié
-        return UserDetailsImpl.build(user);
-        // On retourne une instance de UserDetailsImpl en utilisant l'instance de User trouvée précédemment. Cela est possible grâce à la mthd statique
-        // de fabrication `build`, qui construit une instance de UserDetailsImpl à partir de l'objet User.
+                // si instance non trouvée, une instance de UsernameNotFoundException est créée avec le msg spécifié et rien n'est retourné
+        return UserDetailsImpl.build(user); // retourne une instance de UDI grâce l'instance de User trouvée. Possible grâce au constructeur statique de UDI
     }
-
 }
