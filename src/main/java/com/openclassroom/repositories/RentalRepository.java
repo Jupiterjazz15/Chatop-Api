a82946ab35c37cddadf6cd0e194f0f064c10a2ac
coraliehaller <haller.coralie.15@gmail.com>
2024-11-26 21:48:17 +0100

    Ajout du RentalRepository

---
+++ src/main/java/com/openclassroom/repositories/RentalRepository.java
@@ -0,0 +1,24 @@
+package com.openclassroom.repositories;
+
+import com.openclassroom.models.Rental; // Import du modèle Rental
+import org.springframework.data.jpa.repository.JpaRepository; // Interface JPA pour la gestion des entités
+import org.springframework.stereotype.Repository; // Annotation pour indiquer que c'est un Repository
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RentalRepository extends JpaRepository<Rental, Long> {
+
+    // Méthode pour trouver un rental par son ID (vient directement de JpaRepository  )
+    Optional<Rental> findById(Long id);
+
+    // Méthode pour trouver tous les rentals d'un utilisateur (déduite par Spring Data JPA)
+    List<Rental> findByOwnerId(Long ownerId);
+
+    // Méthode pour rechercher un rental par son nom ou partie (déduite par Spring Data JPA)
+    List<Rental> findByNameContaining(String name);
+
+    // Méthode pour trouver des rentals actifs (déduite par Spring Data JPA)
+    List<Rental> findByAvailable(boolean available);
+}
--- src/main/java/com/openclassroom/repositories/UserRepository.java
+++ src/main/java/com/openclassroom/repositories/UserRepository.java
@@ -4,18 +4,19 @@ import org.springframework.stereotype.Repository; // annotation qui indique que 
 import org.springframework.data.jpa.repository.JpaRepository; // fournit l'interface JpaRepository incluant les mthds pr interagir avec la DB (CRUD, etc.).
 import java.util.Optional; // utilisé pr éviter les retours null et indiquer qu'une valeur peut ou non être présente
 import com.openclassroom.models.User; // import du model User
-import org.springframework.beans.factory.annotation.Autowired; // annotation @Autowired qui permet d'injecter automatiquement des dépendances Spring
-import org.springframework.data.jpa.repository.Query; // annotation utilisée pour définir des requêtes SQL personnalisées.
-import org.springframework.data.repository.query.Param; // annotation  utilisée pour lier les paramètres des requêtes à leurs valeurs.
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     // MTHD DE JPAREPOSITORY
     Optional<User> findByEmail(String email);
+
     // MTHD DE JPAREPOSITORY
     Optional<User>findById(long id);
+
     // MTHD DE JPAREPOSITORY POUR EVITER LES DOUBLONS
     Boolean existsByUsername(String username);
+
     // MTHD DE JPAREPOSITORY POUR EVITER LES DOUBLONS
     Boolean existsByEmail(String email);
 }
